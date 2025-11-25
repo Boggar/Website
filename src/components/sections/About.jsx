31eb5b62ae4badd51e59d3b4a855f3f117318ae0
Manuel Thamm <tha.manuel06@gmail.com>
2025-11-25 18:54:25 +0100

    update: About Section visual overhaul, Projects Section added relevant Projects

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,132 +1,239 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
+  const languageSkills = [
+    "Python",
+    "JavaScript",
+    "Java",
+    "C",
+    "C#",
+    "GDScript",
+    "SQL",
+  ];
 
-    const languageSkills = ["Python", "JavaScript", "Java", "C", "C#", "GDScript", "SQL"];
+  const technologySkills = [
+    "Linux",
+    "Windows",
+    "Whireshark",
+    "SQLDeveloper",
+    "Docker",
+    "MQTT",
+    "YoloCam",
+  ];
 
-    const technologySkills = ["Linux", "Windows", "Whireshark", "SQLDeveloper", "Docker", "MQTT", "YoloCam"];
+  const frameworkSkills = [
+    "React.js",
+    "Node.js",
+    "Spring Boot",
+    "ROS2",
+    "JavaFX",
+    "Java Swing",
+    "Express.js",
+  ];
 
-    const frameworkSkills = ["React.js", "Node.js", "Spring Boot", "ROS2", "JavaFX", "Java Swing", "Express.js"];
+  const securityConcepts = [
+    "XSS",
+    "JWT Auth",
+    "Reverse Shell",
+    "Poisoning",
+    "CSRF",
+    "SQL Injection",
+  ];
 
-    const securityConcepts = ["XSS", "JWT Auth", "Reverse Shell", "Poisoning", "CSRF", "SQL Injection"];
+  const workExperiences = [
+    {
+      title: "Volunteer Worker at Virteum",
+      date: "Mar. 2024 - present",
+      tasks: [
+        "Advising on website features and usability, ensuring practical needs are met",
+        "Supporting the team with everyday IT issues and troubleshooting",
+        "Collaborating with developers to provide guidance on user-focused improvements",
+      ],
+    },
+    {
+      title: "Internship at NDSatCom GmbH",
+      date: "Mar. 2025 - Aug. 2025",
+      tasks: [
+        "Security checks of company products",
+        "Programming GUI elements in Java Swing/FX",
+      ],
+    },
+    {
+      title: "Liebherr Hackathon",
+      date: "Oct. - Nov. 2024",
+      tasks: [
+        "Main Round 6th Place (21st Nov. 2024)",
+        "Qualification Round 2nd Place (24th Oct. 2024)",
+      ],
+    },
+  ];
 
-    return <section id="about" className="min-h-screen flex items-center justify-center py-20"
+  const education = [
+    {
+      title: "B.S. in Computer Science",
+      date: "Oct. 2021 - Jul. 2026",
+      details: "Ravensburg-Weingarten University of Applied Sciences",
+      coursework: [
+        "Data Structures",
+        "Data Security",
+        "System Security",
+        "System Programming",
+        "System Administration",
+        "Cloud Computing",
+        "Operating Systems",
+        "Network Engineering",
+        "Software Development",
+        "Databse Programming",
+      ],
+    },
+    {
+      title: "Exchange Semester",
+      date: "Jul. 2023 - Dec. 2023",
+      details: "Seinäjoki University of Applied Sciences",
+      coursework: [
+        "Basics of Electronics",
+        "Robotics",
+        "Automation Technology",
+      ],
+    },
+  ];
+
+  return (
+    <section
+      id="about"
+      className="min-h-screen flex items-center justify-center py-20"
     >
-        <RevealOnScroll>
+      <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4">
-            <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-700 to-amber-400 bg-clip-text text-transparent text-center">
-                {" "}
-                About Me
-            </h2>
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-700 to-amber-400 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
 
-            <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-                <p className="text-gray-300 mb-6">
-                    Dedicated student of Applied Computer Science with a keen interest in IT security and initial practical experience
-                    from a cybersecurity hackathon. Familiar with topics such as web vulnerabilities (XSS, CSRF), authentication mechanisms (JWT),
-                    and basic attack techniques (reverse shells, poisoning, SQLi).
-                </p>
+          <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Dedicated student of Applied Computer Science with a keen interest
+              in IT security and initial practical experience from a
+              cybersecurity hackathon. Familiar with topics such as web
+              vulnerabilities (XSS, CSRF), authentication mechanisms (JWT), and
+              basic attack techniques (reverse shells, poisoning, SQLi).
+            </p>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4">Languages</h3>
-                        <div className="flex flex-wrap gap-2">
-                            {languageSkills.map((tech, key) => (
-                                <span
-                                    key={key}
-                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                    {tech}
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-
-                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4">Tools & Platforms</h3>
-                        <div className="flex flex-wrap gap-2">
-                            {technologySkills.map((tech, key) => (
-                                <span
-                                    key={key}
-                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                    {tech}
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-                        
-                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4">Framework & Libraries</h3>
-                        <div className="flex flex-wrap gap-2">
-                            {frameworkSkills.map((tech, key) => (
-                                <span
-                                    key={key}
-                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                    {tech}
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-                        
-                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4">Security Concepts</h3>
-                        <div className="flex flex-wrap gap-2">
-                            {securityConcepts.map((tech, key) => (
-                                <span
-                                    key={key}
-                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                    {tech}
-                                </span>
-                            ))}
-                        </div>
-                    </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Languages</h3>
+                <div className="flex flex-wrap gap-2">
+                  {languageSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                    >
+                      {tech}
+                    </span>
+                  ))}
                 </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                    <h3 className="text-xl font-bold mb-4"> Education </h3>
-                    <ul className="list-disc list-inside text-gray-300 space-y-2">
-                        <li>
-                            <strong> B.S. in Computer Science </strong> - Ravensburg-Weingarten University of Education (Oct. 2021 - Jul. 2026)
-                        </li>
-                        
-                        <li>
-                            <strong>Exchange Semester </strong> - Seinäjoki University of Applied Sciences (Jul. 2023 - Dec. 2023)
-                        </li>
+              </div>
 
-                        <li>
-                            <strong>Relevant Coursework:</strong> Data Structures, System Security, Data Security, System Administration,
-                            Cloud Computing, Operating Systems, Network Engineering and Software Development
-                        </li>
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Tools & Platforms</h3>
+                <div className="flex flex-wrap gap-2">
+                  {technologySkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">
+                  Framework & Libraries
+                </h3>
+                <div className="flex flex-wrap gap-2">
+                  {frameworkSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Security Concepts</h3>
+                <div className="flex flex-wrap gap-2">
+                  {securityConcepts.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-8 mt-8">
+            <div className="flex flex-col gap-6">
+              <h3 className="text-xl font-bold mb-4">Education</h3>
+              {education.map((uni, idx) => (
+                <div
+                  key={idx}
+                  className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all"
+                >
+                  <h4 className="text-lg font-bold text-purple-600">
+                    {uni.title}
+                  </h4>
+                  <p className="text-gray-300 mb-2">{uni.details}</p>
+                  <p className="text-sm text-gray-300 italic mb-2">
+                    {uni.date}
+                  </p>
+                  <p className="text-gray-300 mb-2">Relevant Coursework: </p>
+                  {uni.coursework.length > 0 && (
+                    <ul className="list-disc list-inside text-gray-300 space-y-1">
+                      {uni.coursework.map((task, i) => (
+                        <li key={i}>{task}</li>
+                      ))}
                     </ul>
+                  )}
                 </div>
+              ))}
+            </div>
 
-                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                    <h3 className="text-xl font-bold mb-4"> Work Experience </h3>
-                    <div className="space-y-4 text-gray-300">
-                        <div>
-                            <h4>
-                                <strong>Internship at NDSatcom GmbH</strong> - (Mar. 2025 - Aug. 2025)
-                                <ul className="list-disc list-inside text-gray-300 space-y-2">
-                                    <li>Security Checks of comanpy products</li>
-                                    <li>Programming GUI elements in Java Swing/FX</li>
-                                </ul>
-                            </h4>
-
-                            <h4>
-                                    <strong>Liebherr Hackathon</strong>
-                                    <ul className="list-disc list-inside text-gray-300 space-y-2">
-                                    <li>Main Round 6th Place (21st Nov. 2024)</li>   
-                                    <li>Qualification Round 2nd Place (24th Oct. 2024)</li>
-                                </ul>
-                            </h4>
-                        </div>
-                    </div>
+            <div className="flex flex-col gap-6">
+              <h3 className="text-xl font-bold mb-4">Work Experience</h3>
+              {workExperiences.map((job, idx) => (
+                <div
+                  key={idx}
+                  className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all"
+                >
+                  <h4 className="text-lg font-bold text-purple-600">
+                    {job.title}
+                  </h4>
+                  <p className="text-sm text-gray-300 italic mb-2">
+                    {job.date}
+                  </p>
+                  <ul className="list-disc list-inside text-gray-300 space-y-1">
+                    {job.tasks.map((task, i) => (
+                      <li key={i}>{task}</li>
+                    ))}
+                  </ul>
                 </div>
+              ))}
             </div>
-            </div>
-            </RevealOnScroll>
+          </div>
+        </div>
+      </RevealOnScroll>
     </section>
-}
+  );
+};
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,77 +1,126 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Projects = () => {
-    return (
-        <section id="projects" className="min-h-screen flex items-center justify-center py-20">
-            <RevealOnScroll>
-            <div className="max-w-5xl mx-auto px-4">
-                <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-700 to-amber-400 bg-clip-text text-transparent text-center">
-                    {" "}
-                    Featured Projects
-                </h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-1-y hover:border-purple-500/30 hover:sahdow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-                        <h3 className="text-xl font-bold mb-2">Placeholder 1</h3>
-                        <p className="text-gray-400 mb-4">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-                        </p>
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Nodes.js", "AWS", "Docker"].map((tech, key) => (
-                                <span key={key}
-                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                    {tech}
-                                </span>
-                            ))}
-                        </div>
-                        <div className="flex justify-between items-center">
-                            <a href="" className="text-purple-400 hover:text-purple-300 transition-colors my-4">View Project →</a>
-                            </div>
-                    </div>
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-700 to-amber-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-1-y hover:border-purple-500/30 hover:sahdow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Python Keylogger</h3>
+              <p className="text-gray-400 mb-4">
+                A minimalistic Python keylogger, developed solely for
+                educational and demonstration purposes. The project shows how
+                global keystrokes can be intercepted and stored locally using
+                pynput.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {[
+                  "Event Handling",
+                  "Data Operations",
+                  "System Programming",
+                  "Python",
+                ].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/Boggar/Keylogger"
+                  className="text-purple-400 hover:text-purple-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
 
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-1-y hover:border-purple-500/30 hover:sahdow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-                        <h3 className="text-xl font-bold mb-2">Placeholder 2</h3>
-                        <p className="text-gray-400 mb-4">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-                        </p>
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Nodes.js", "AWS", "Docker"].map((tech, key) => (
-                                <span key={key}
-                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                    {tech}
-                                </span>
-                            ))}
-                        </div>
-                        <div className="flex justify-between items-center">
-                            <a href="" className="text-purple-400 hover:text-purple-300 transition-colors my-4">View Project →</a>
-                            </div>
-                    </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-1-y hover:border-purple-500/30 hover:sahdow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">OCR Detection Tool</h3>
+              <p className="text-gray-400 mb-4">
+                A Python-based OCR tool with a user-friendly GUI for extracting
+                text from PDFs and image files (.png, .jpg, .jpeg, .tiff, etc.).
+                It converts PDFs to images (pdf2image), recognizes text using
+                pytesseract, and exports results as PDF files (ReportLab). The
+                GUI is built with PySimpleGUI to select files, set output
+                folders, and choose languages.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {[
+                  "OCR Processing",
+                  "Multi-Threading",
+                  "File-Handling",
+                  "GUI Design",
+                  "Python",
+                ].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/Boggar/OCR_Detection"
+                  className="text-purple-400 hover:text-purple-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
 
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-1-y hover:border-purple-500/30 hover:sahdow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
-                        <h3 className="text-xl font-bold mb-2">Placeholder 3</h3>
-                        <p className="text-gray-400 mb-4">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-                        </p>
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Nodes.js", "AWS", "Docker"].map((tech, key) => (
-                                <span key={key}
-                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
-                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
-                                    {tech}
-                                </span>
-                            ))}
-                        </div>
-                        <div className="flex justify-between items-center">
-                            <a href="" className="text-purple-400 hover:text-purple-300 transition-colors my-4">View Project →</a>
-                            </div>
-                    </div>
-                </div>
-                </div>
-                </RevealOnScroll>
-        </section>
-    )
-}
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-1-y hover:border-purple-500/30 hover:sahdow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Smart Alarmclock</h3>
+              <p className="text-gray-400 mb-4">
+                A smart alarm clock that uses image recognition to detect
+                whether a person is still in bed. As long as the person does not
+                get up, the alarm tone becomes more annoying. As soon as the
+                person gets up, the alarm stops. Optionally, a Telegram bot can
+                notify the person that they have successfully gotten up. The
+                alarm times are set via Google Calendar.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Python", "YoloCam", "MQTT", "Docker"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href=""
+                  className="text-purple-400 hover:text-purple-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
